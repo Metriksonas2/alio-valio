a4c835a6e53d3e30356935a8c4c4058c76cbbe62
Aldas <aldas.pakenas@ktu.edu>
2021-12-12 20:06:51 +0200

    Sutvarkytas frontas, padarytas dropdownas user'io

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
@@ -13,5 +14,16 @@ class TestController extends AbstractController
     public function index()
     {
         return $this->render('test/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/add/edit/{ad}", name="app_ad_edit")
+     */
+    public function edit($ad, Request $request)
+    {
+        if ($this->getUser())
+        {
+            return $this->redirectToRoute("app_");
+        }
     }
 }
