f7c2c15fae1a3ac39e011008582dc310121ded8a
Aldas <aldas.pakenas@ktu.edu>
2021-12-12 20:59:30 +0200

    PR fix

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -15,15 +15,4 @@ class TestController extends AbstractController
     {
         return $this->render('test/index.html.twig', []);
     }
-
-    /**
-     * @Route("/add/edit/{ad}", name="app_ad_edit")
-     */
-    public function edit($ad, Request $request)
-    {
-        if ($this->getUser())
-        {
-            return $this->redirectToRoute("app_");
-        }
-    }
 }
