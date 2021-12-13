7e5750c5421ce2c1c82f091d9c34cd78645806d2
Metriksonas2 <erikas.goriacevskis@gmail.com>
2021-12-13 11:41:13 +0200

    Newest updates

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TestController extends AbstractController
+class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_index")
@@ -17,7 +17,7 @@ class TestController extends AbstractController
     public function index(Request $request, AdConfigService $adConfigService, EntityManagerInterface $entityManager)
     {
         if (!$this->getUser()) {
-            return $this->render('test/guest.html.twig', []);
+            return $this->render('home/guest.html.twig', []);
         }
 
         $adRepository = $entityManager->getRepository(Ad::class);
@@ -25,7 +25,7 @@ class TestController extends AbstractController
         $ads = $adConfigService->getAdsForHomePage($request, $adRepository);
         $ads = array_reverse($ads);
 
-        return $this->render('test/index.html.twig', [
+        return $this->render('home/index.html.twig', [
             'ads' => $ads
         ]);
     }
